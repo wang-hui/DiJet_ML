a3b169374e6c1aed68ffd0f62113551f4724e9c1
hui wang wang <66718889@qq.com>
2023-10-21 08:17:25 -0500

    add rho variable

--- Add_to_TTree.py
+++ Add_to_TTree.py
@@ -3,8 +3,8 @@ ROOT.ROOT.EnableImplicitMT()
 ROOT.gInterpreter.Declare('#include "Add_to_TTree.h"')
 
 #InputList = [500]
-#InputList = [500, 600, 700, 800, 900, 1000, 1250, 1500, 1750, 2000, 2500, 3000]
-InputList = ["QCD_1M_stride70"]
+InputList = [500, 600, 700, 800, 900, 1000, 1250, 1500, 1750, 2000, 2500, 3000]
+#InputList = ["QCD_1M_stride70"]
 
 InputDir = "/eos/uscms/store/user/huiwang/Dijet/ML_TTree/"
 OutputDir = "ML_TTree/"
@@ -14,24 +14,31 @@ LvecJ2 = LvecJ1.replace("jet1", "jet2")
 LvecJ3 = LvecJ1.replace("jet1", "jet3")
 LvecJ4 = LvecJ1.replace("jet1", "jet4")
 
-SaveList = ["Mass", "weight", "fourjetmasstev",
+SaveList = ["Mass", "weight", "evt_trig",
+        "fourjetmasstev", "Mjj_avg_dRpairing_GeV",
 
         "Mjj_msortedP1_high_div4jm", "Mjj_msortedP1_low_div4jm",
         "Mjj_msortedP2_high_div4jm", "Mjj_msortedP2_low_div4jm",
         "Mjj_msortedP3_high_div4jm", "Mjj_msortedP3_low_div4jm",
 
-        "P1_Mhigh_TeV", "P1_Mlow_TeV",
-        "P2_Mhigh_TeV", "P2_Mlow_TeV",
-        "P3_Mhigh_TeV", "P3_Mlow_TeV",
-
-        "P1_omega", "P2_omega", "P3_omega",
-        "P1_x", "P2_x", "P3_x",
-
-        "Mjj_avg_dRpairing_GeV", "evt_trig",
+        "P1high_MTeV", "P1low_MTeV",
+        "P2high_MTeV", "P2low_MTeV",
+        "P3high_MTeV", "P3low_MTeV",
 
         "P1high_j1", "P1high_j2", "P1low_j1", "P1low_j2",
         "P2high_j1", "P2high_j2", "P2low_j1", "P2low_j2",
         "P3high_j1", "P3high_j2", "P3low_j1", "P3low_j2",
+
+        "P1_omega", "P2_omega", "P3_omega",
+        "P1_x", "P2_x", "P3_x",
+
+        "P1high_rho_high", "P1high_rho_low", "P1low_rho_high", "P1low_rho_low",
+        "P2high_rho_high", "P2high_rho_low", "P2low_rho_high", "P2low_rho_low",
+        "P3high_rho_high", "P3high_rho_low", "P3low_rho_high", "P3low_rho_low",
+
+        "P1high_rho", "P1low_rho",
+        "P2high_rho", "P2low_rho",
+        "P3high_rho", "P3low_rho",
 ]
 
 for Input in InputList:
@@ -50,16 +57,16 @@ for Input in InputList:
         RDF = RDF.Define("Mass", str(Input))
         RDF = RDF.Define("weight", str(1))
 
-    RDF = RDF.Define("P1_Mhigh_TeV", "Mjj_msortedP1_high / 1000")
-    RDF = RDF.Define("P1_Mlow_TeV", "Mjj_msortedP1_low / 1000")
-    RDF = RDF.Define("P2_Mhigh_TeV", "Mjj_msortedP2_high / 1000")
-    RDF = RDF.Define("P2_Mlow_TeV", "Mjj_msortedP2_low / 1000")
-    RDF = RDF.Define("P3_Mhigh_TeV", "Mjj_msortedP3_high / 1000")
-    RDF = RDF.Define("P3_Mlow_TeV", "Mjj_msortedP3_low / 1000")
+    RDF = RDF.Define("P1high_MTeV", "Mjj_msortedP1_high / 1000")
+    RDF = RDF.Define("P1low_MTeV", "Mjj_msortedP1_low / 1000")
+    RDF = RDF.Define("P2high_MTeV", "Mjj_msortedP2_high / 1000")
+    RDF = RDF.Define("P2low_MTeV", "Mjj_msortedP2_low / 1000")
+    RDF = RDF.Define("P3high_MTeV", "Mjj_msortedP3_high / 1000")
+    RDF = RDF.Define("P3low_MTeV", "Mjj_msortedP3_low / 1000")
 
-    RDF = RDF.Define("Mjj_msortedP1_low_div4jm", "P1_Mlow_TeV / fourjetmasstev")
-    RDF = RDF.Define("Mjj_msortedP2_low_div4jm", "P2_Mlow_TeV / fourjetmasstev")
-    RDF = RDF.Define("Mjj_msortedP3_low_div4jm", "P3_Mlow_TeV / fourjetmasstev")
+    RDF = RDF.Define("Mjj_msortedP1_low_div4jm", "P1low_MTeV / fourjetmasstev")
+    RDF = RDF.Define("Mjj_msortedP2_low_div4jm", "P2low_MTeV / fourjetmasstev")
+    RDF = RDF.Define("Mjj_msortedP3_low_div4jm", "P3low_MTeV / fourjetmasstev")
 
     RDF = RDF.Define("j1lvec", "ROOT::Math::PtEtaPhiMVector(" + LvecJ1 + ")")
     RDF = RDF.Define("j2lvec", "ROOT::Math::PtEtaPhiMVector(" + LvecJ2 + ")")
@@ -92,9 +99,42 @@ for Input in InputList:
     RDF = RDF.Define("P2_omega", "abs( tanh(  (P2high.Rapidity() - P2low.Rapidity())/2  ) )")
     RDF = RDF.Define("P3_omega", "abs( tanh(  (P3high.Rapidity() - P3low.Rapidity())/2  ) )")
 
-    RDF = RDF.Define("P1_x", "-1 + fourjetmasstev / (P1_Mhigh_TeV + P1_Mlow_TeV)")
-    RDF = RDF.Define("P2_x", "-1 + fourjetmasstev / (P2_Mhigh_TeV + P2_Mlow_TeV)")
-    RDF = RDF.Define("P3_x", "-1 + fourjetmasstev / (P3_Mhigh_TeV + P3_Mlow_TeV)")
+    RDF = RDF.Define("P1_x", "-1 + fourjetmasstev / (P1high_MTeV + P1low_MTeV)")
+    RDF = RDF.Define("P2_x", "-1 + fourjetmasstev / (P2high_MTeV + P2low_MTeV)")
+    RDF = RDF.Define("P3_x", "-1 + fourjetmasstev / (P3high_MTeV + P3low_MTeV)")
+
+    RDF = RDF.Define("P1high_rho_j1", "abs(1 - 2 * TMath::Sq( (P1high + P1low_j1).M()/1000 / fourjetmasstev) )")
+    RDF = RDF.Define("P1high_rho_j2", "abs(1 - 2 * TMath::Sq( (P1high + P1low_j2).M()/1000 / fourjetmasstev) )")
+    RDF = RDF.Define("P1low_rho_j1", "abs(1 - 2 * TMath::Sq( (P1low + P1high_j1).M()/1000 / fourjetmasstev) )")
+    RDF = RDF.Define("P1low_rho_j2", "abs(1 - 2 * TMath::Sq( (P1low + P1high_j2).M()/1000 / fourjetmasstev) )")
+    RDF = RDF.Define("P2high_rho_j1", "abs(1 - 2 * TMath::Sq( (P2high + P2low_j1).M()/1000 / fourjetmasstev) )")
+    RDF = RDF.Define("P2high_rho_j2", "abs(1 - 2 * TMath::Sq( (P2high + P2low_j2).M()/1000 / fourjetmasstev) )")
+    RDF = RDF.Define("P2low_rho_j1", "abs(1 - 2 * TMath::Sq( (P2low + P2high_j1).M()/1000 / fourjetmasstev) )")
+    RDF = RDF.Define("P2low_rho_j2", "abs(1 - 2 * TMath::Sq( (P2low + P2high_j2).M()/1000 / fourjetmasstev) )")
+    RDF = RDF.Define("P3high_rho_j1", "abs(1 - 2 * TMath::Sq( (P3high + P3low_j1).M()/1000 / fourjetmasstev) )")
+    RDF = RDF.Define("P3high_rho_j2", "abs(1 - 2 * TMath::Sq( (P3high + P3low_j2).M()/1000 / fourjetmasstev) )")
+    RDF = RDF.Define("P3low_rho_j1", "abs(1 - 2 * TMath::Sq( (P3low + P3high_j1).M()/1000 / fourjetmasstev) )")
+    RDF = RDF.Define("P3low_rho_j2", "abs(1 - 2 * TMath::Sq( (P3low + P3high_j2).M()/1000 / fourjetmasstev) )")
+
+    RDF = RDF.Define("P1high_rho_high", "TMath::Max(P1high_rho_j1, P1high_rho_j2)")
+    RDF = RDF.Define("P1high_rho_low", "TMath::Min(P1high_rho_j1, P1high_rho_j2)")
+    RDF = RDF.Define("P1low_rho_high", "TMath::Max(P1low_rho_j1, P1low_rho_j2)")
+    RDF = RDF.Define("P1low_rho_low", "TMath::Min(P1low_rho_j1, P1low_rho_j2)")
+    RDF = RDF.Define("P2high_rho_high", "TMath::Max(P2high_rho_j1, P2high_rho_j2)")
+    RDF = RDF.Define("P2high_rho_low", "TMath::Min(P2high_rho_j1, P2high_rho_j2)")
+    RDF = RDF.Define("P2low_rho_high", "TMath::Max(P2low_rho_j1, P2low_rho_j2)")
+    RDF = RDF.Define("P2low_rho_low", "TMath::Min(P2low_rho_j1, P2low_rho_j2)")
+    RDF = RDF.Define("P3high_rho_high", "TMath::Max(P3high_rho_j1, P3high_rho_j2)")
+    RDF = RDF.Define("P3high_rho_low", "TMath::Min(P3high_rho_j1, P3high_rho_j2)")
+    RDF = RDF.Define("P3low_rho_high", "TMath::Max(P3low_rho_j1, P3low_rho_j2)")
+    RDF = RDF.Define("P3low_rho_low", "TMath::Min(P3low_rho_j1, P3low_rho_j2)")
+
+    RDF = RDF.Define("P1high_rho", "(P1high_rho_j1 + P1high_rho_j2) / 2")
+    RDF = RDF.Define("P1low_rho", "(P1low_rho_j1 + P1low_rho_j2) / 2")
+    RDF = RDF.Define("P2high_rho", "(P2high_rho_j1 + P2high_rho_j2) / 2")
+    RDF = RDF.Define("P2low_rho", "(P2low_rho_j1 + P2low_rho_j2) / 2")
+    RDF = RDF.Define("P3high_rho", "(P3high_rho_j1 + P3high_rho_j2) / 2")
+    RDF = RDF.Define("P3low_rho", "(P3low_rho_j1 + P3low_rho_j2) / 2")
 
     OutCols = ROOT.vector("string")()
     for Col in SaveList: OutCols.push_back(Col)
--- Sanity_Plots.py
+++ Sanity_Plots.py
@@ -8,69 +8,69 @@ MassList = [500, 600, 700, 800, 900, 1000, 1250, 1500, 1750, 2000, 2500, 3000]
 InputDir = "ML_TTree/"
 ResultsDir = "results_temp/"
 
+class PlotCfg:
+    def __init__(self, Xval, Xtitle, Xbins, Xlow, Xhigh, Yval, Ytitle, Ybins, Ylow, Yhigh, CfgList):
+        self.Xval = Xval
+        self.Xtitle = Xtitle
+        self.Xbins = Xbins
+        self.Xlow = Xlow
+        self.Xhigh = Xhigh
+        self.Yval = Yval
+        self.Ytitle = Ytitle
+        self.Ybins = Ybins
+        self.Ylow = Ylow
+        self.Yhigh = Yhigh
+        CfgList.append(self)
+
+CfgList = []
+#Mjj_vs_M4j = PlotCfg()
+x_vs_omega = PlotCfg("_omega", "", 100, 0, 1, "_x", "", 100, 0, 1.6, CfgList)
+Phigh_rho = PlotCfg("high_rho_high", "", 100, 0, 1, "high_rho_low", "", 100, 0, 1, CfgList)
+Plow_rho = PlotCfg("low_rho_high", "", 100, 0, 1, "low_rho_low", "", 100, 0, 1, CfgList)
+x_vs_Phigh_rho  = PlotCfg("high_rho", "", 100, 0, 1, "_x", "", 100, 0, 1.6, CfgList)
+x_vs_Plow_rho  = PlotCfg("low_rho", "", 100, 0, 1, "_x", "", 100, 0, 1.6, CfgList)
+
+Trig = ""
+Trig = "PassTrig"
+
 MyChain = ROOT.TChain("tree_ML")
 for Mass in MassList:
-    RootFile = "tree_ML_MCRun2_" + str(Mass) + "GeV.root"
-    print "processing ", RootFile
-    MyChain.Add(InputDir + RootFile)
+    FileName = "tree_ML_MCRun2_" + str(Mass) + "GeV.root"
+    MyChain.Add(InputDir + FileName)
 
 RDF = ROOT.RDataFrame(MyChain)
+if Trig != "": RDF = RDF.Filter("evt_trig == 1")
 
-MassPlotList = MassList + [-1]
-for Idx, MassPlot in enumerate(MassPlotList):
+if len(MassList) > 1 : MassList += [-1]
+for Mass in MassList:
+    print "processing", str(Mass), "GeV"
     RDFTemp = RDF
     Title = "Gen mass all"
-    Xlow = 0
-    Xhigh = 10
-    if MassPlot != -1:
-        RDFTemp = RDFTemp.Filter("Mass == " + str(MassPlot))
-        Title = Title.replace("all", str(MassPlot) + " GeV")
-        Xlow = MassPlot/1000.0
-        Xhigh = MassPlot/1000.0 + 2.6 + 0.4 * Idx
+    if Mass != -1:
+        RDFTemp = RDFTemp.Filter("Mass == " + str(Mass))
+        Title = Title.replace("all", str(Mass) + " GeV")
 
     for P in range(3):
         Pair = "P" + str(P+1)
 
-        ######### mjj vs m4j #########
-        Mjj = "Mjj_msorted" + Pair + "_high_div4jm"
-        H2dModel = ROOT.RDF.TH2DModel(Title, Title, 200, Xlow, Xhigh, 200, 0.45 - 0.2 * P, 0.8 - 0.1 * P)
-        H2d = RDFTemp.Histo2D(H2dModel, "fourjetmasstev", Mjj)
-        H2d.GetXaxis().SetTitle("M_{4j} [TeV]")
-        H2d.GetYaxis().SetTitle(Mjj)
-    
-        H2dPfX = H2d.ProfileX()
-        H2dPfX.Rebin(4)
-        H2dPfX.SetLineColor(ROOT.kRed)
-    
-        H2dPfY = H2d.ProfileY()
-        H2dPfY.Rebin(4)
-        H2dPfY.SetLineColor(ROOT.kRed)
-    
         MyCanvas = ROOT.TCanvas("MyCanvas", "MyCanvas", 600, 600)
         MyCanvas.SetLeftMargin(0.15)
         MyCanvas.SetRightMargin(0.15)
-    
-        H2dPfY.Draw()
-        MyCanvas.SaveAs(ResultsDir + Mjj + "_vs_M4j_PfY_" + str(MassPlot) + "GeV.png")
-    
         MyCanvas.SetLogz()
-        H2d.Draw("colz")
-        H2dPfX.Draw("same")
+
+        for Cfg in CfgList:
+            Xval = Pair + Cfg.Xval
+            Yval = Pair + Cfg.Yval
+            H2dModel = ROOT.RDF.TH2DModel(Title, Title, Cfg.Xbins, Cfg.Xlow, Cfg.Xhigh,
+                                                        Cfg.Ybins, Cfg.Ylow, Cfg.Yhigh)
+            H2d = RDFTemp.Histo2D(H2dModel, Xval, Yval)
+            H2d.GetXaxis().SetTitle(Xval)
+            H2d.GetYaxis().SetTitle(Yval)
     
-        MyCanvas.SaveAs(ResultsDir + Mjj + "_vs_M4j" + str(MassPlot) + "GeV.png")
-
-        ######### x vs omega #########
-        Omega = Pair + "_omega"
-        X = Pair + "_x"
-        H2dModel = ROOT.RDF.TH2DModel(Title, Title, 100, 0, 1, 100, 0, 1.5)
-        H2d = RDFTemp.Histo2D(H2dModel, Omega, X)
-        H2d.GetXaxis().SetTitle(Omega)
-        H2d.GetYaxis().SetTitle(X)
-
-        H2dPfX = H2d.ProfileX()
-        H2dPfX.SetLineColor(ROOT.kRed)
-
-        H2d.Draw("colz")
-        H2dPfX.Draw("same")
-
-        MyCanvas.SaveAs(ResultsDir + X + "_vs_" + Omega + "_"+ str(MassPlot) + "GeV.png") 
+            H2dPfX = H2d.ProfileX()
+            H2dPfX.SetLineColor(ROOT.kRed)
+    
+            H2d.Draw("colz")
+            H2dPfX.Draw("same")
+    
+            MyCanvas.SaveAs(ResultsDir + Yval + "_vs_" + Xval + "_" + Trig + str(Mass) + "GeV.png")
