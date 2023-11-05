d730b38809cdb3fed62f751fb80d940eb6201ae7
hui wang wang <66718889@qq.com>
2023-11-05 15:58:02 -0600

    update plot code

---
+++ Plot_compare.py
@@ -0,0 +1,149 @@
+#lowercase with underline for function/class names
+#canmel case for variables
+
+import ROOT as rt
+
+BaseName = "trigger"
+BaseFileList = ["Hists_all_cuts.root"]
+BaseHistList = ["ML_Efficiency_Trig"]
+
+Comp1Name = "trigger + massAsymm"
+#Comp1FileList = BaseFileList
+Comp1FileList = ["Hists_skip_geo_cuts.root"]
+#Comp1HistList = BaseHistList
+Comp1HistList = ["ML_Efficiency_Masym"]
+
+Comp2Name = "trigger + dR + dEta + massAsymm"
+Comp2FileList = BaseFileList
+Comp2FileList = ["Hists_all_cuts.root"]
+Comp2HistList = BaseHistList
+Comp2HistList = ["ML_Efficiency_Masym"]
+
+Comp3Name = "My new, PU"
+Comp3FileList = BaseFileList
+Comp3FileList = ["My_zeroOut_PU_respCorr_ieta26.root"]
+Comp3HistList = BaseHistList
+#Comp3HistList = ["myCaloMETBE_h"]
+
+ShapeComp = False
+SetLogY = False
+
+YTitle = "Efficiency"
+XTitle = "Gen Mass [GeV]"
+#XTitle = "nConstituents"
+
+InputList = [500, 600, 700, 800, 900, 1000, 1250, 1500, 1750, 2000, 2500, 3000]
+
+XMin = 0
+XMax = 0
+YScale = 1.5
+
+FileDir = "results_temp/"
+HistDir = ""
+
+if ShapeComp: YTitle = "A.U."
+
+class MyStruct:
+    def __init__(self, Name, FileList, HistList, Color, StructList):
+        self.Name = Name
+        self.FileList = FileList
+        self.HistList = HistList
+        self.Color = Color
+        StructList.append(self)
+
+StructList = []
+Base = MyStruct(BaseName, BaseFileList, BaseHistList, rt.kBlue, StructList)
+Comp1 = MyStruct(Comp1Name, Comp1FileList, Comp1HistList, rt.kRed, StructList)
+Comp2 = MyStruct(Comp2Name, Comp2FileList, Comp2HistList, rt.kGreen+1, StructList)
+#Comp3 = MyStruct(Comp3Name, Comp3FileList, Comp3HistList, rt.kYellow+1, StructList)
+
+rt.TH1.AddDirectory(rt.kFALSE)
+#rt.TH1.__init__._creates = False
+
+for i in range(len(BaseFileList)):
+    for j in range(len(BaseHistList)):
+        MyCanvas = rt.TCanvas("MyCanvas", "MyCanvas", 600, 600)
+        rt.gStyle.SetOptStat(rt.kFALSE)
+
+        MyLeg = rt.TLegend(0.3,0.65,0.9,0.9)
+
+        PadUp = rt.TPad("PadUp", "PadUp", 0, 0.3, 1, 1.0)
+        PadUp.SetBottomMargin(0.01)
+        PadUp.Draw()
+
+        PadDown = rt.TPad("PadDown", "PadDown", 0, 0, 1, 0.3)
+        PadDown.SetTopMargin(0.03)
+        PadDown.SetBottomMargin(0.3)
+        PadDown.SetGrid()
+        PadDown.Draw()
+
+        OutName = BaseFileList[i].replace(".root", "_")
+        BaseHist = None
+
+        for k in range(len(StructList)):
+            iFileName = StructList[k].FileList[i]
+            iHistName = StructList[k].HistList[j]
+            OutName = OutName + iHistName.replace("_h", "_")
+            iFile = rt.TFile.Open(FileDir + iFileName)
+            iHist = iFile.Get(HistDir + iHistName)
+            print k, iHist
+            iHist.SetLineColor(StructList[k].Color)
+            iHist.Sumw2()
+            if ShapeComp: iHist.Scale(1/iHist.GetEntries())
+            MyLeg.AddEntry(iHist, StructList[k].Name, "l")
+
+            MyCanvas.cd()
+            PadUp.cd()
+
+            YMaxTemp = iHist.GetMaximum() * YScale
+            if k == 0:
+                BaseHist = iHist.Clone()
+
+                BaseHist.GetYaxis().SetTitle(YTitle)
+                BaseHist.SetMaximum(YMaxTemp)
+                BaseHist.SetMinimum(0)
+                BaseHist.SetTitle("")
+                if XMax > 0:
+                    BaseHist.GetXaxis().SetRangeUser(XMin, XMax)
+                BaseHist.Draw("hist")
+
+                MyCanvas.cd()
+                PadDown.cd()
+                BaseFrame = BaseHist.Clone()
+                BaseFrame.Reset()
+
+                BaseFrame.GetYaxis().SetTitle("Ratio")
+                BaseFrame.GetYaxis().SetTitleOffset(0.4)
+                BaseFrame.GetYaxis().SetTitleSize(0.1)
+                BaseFrame.GetYaxis().SetLabelSize(0.08)
+                BaseFrame.GetYaxis().SetRangeUser(0, 2)
+
+                BaseFrame.GetXaxis().SetTitle(XTitle)
+                BaseFrame.GetXaxis().SetTitleOffset(0.8)
+                BaseFrame.GetXaxis().SetTitleSize(0.1)
+                BaseFrame.GetXaxis().SetLabelSize(0.08)
+                if len(InputList) > 0:
+                    for Bin, Lable in enumerate(InputList):
+                        BaseFrame.GetXaxis().SetBinLabel(Bin+1, str(Lable))
+                BaseFrame.Draw()
+
+                MyLine = rt.TLine(BaseFrame.GetXaxis().GetXmin(), 1.0, BaseFrame.GetXaxis().GetXmax(), 1.0)
+                MyLine.Draw()
+            else:
+                if YMaxTemp > BaseHist.GetMaximum(): 
+                    BaseHist.SetMaximum(YMaxTemp)
+                iHist.Draw("histsame")
+
+                MyCanvas.cd()
+                PadDown.cd()
+                RatioHist = iHist.Clone()
+                rt.SetOwnership(RatioHist, False)
+                RatioHist.Divide(BaseHist)
+                RatioHist.Draw("histsame")
+
+        MyCanvas.cd()
+        PadUp.cd()
+        if SetLogY: PadUp.SetLogy()
+        MyLeg.Draw()
+        MyCanvas.SaveAs("results_temp/" + OutName + ".png")
+
--- Result_Plots.py
+++ Result_Plots.py
@@ -52,9 +52,10 @@ def evaluate_rdf (RDF, Name, Mass):
     MyLeg.AddEntry(ML_pred_M.GetPtr(), "ML pred", "l")
 
     if Mass > 0:
-        ML_pred_M.SetTitle("Gen mass " + str(Mass) + "GeV")
         Truth_QSMD_M.Draw("same")
         MyLeg.AddEntry(Truth_QSMD_M.GetPtr(), "truth", "l")
+        ML_pred_M.SetTitle("Gen mass " + str(Mass) + "GeV")
+        ML_pred_M.SetMaximum(Truth_QSMD_M.GetMaximum() * 1.2)
 
         EventsTot = RDF.Count().GetValue()
         Mu, Sigma = fit_gaus(Truth_QSMD_M)
@@ -63,23 +64,23 @@ def evaluate_rdf (RDF, Name, Mass):
         Truth_Efficiency = Truth_QSMD_M_3Sigma / EventsTot
         ML_pred_M_3Sigma = ML_pred_M.Integral(ML_pred_M.FindBin(Mu-3*Sigma), ML_pred_M.FindBin(Mu+3*Sigma))
         ML_Efficiency = ML_pred_M_3Sigma /  EventsTot
-    
+
         RDF_temp = RDF.Filter("ML_pred == Truth_QSMD")
         EventsMatch = RDF_temp.Count().GetValue()
         ML_Accuracy = float(EventsMatch) / EventsTot
-    
+
         print ("ML accuracy: %.2f" %ML_Accuracy, "Truth efficiency: %.2f" %Truth_Efficiency,
                 "ML efficiency: %.2f" %ML_Efficiency)
-    
+
     MyLeg.Draw()
-    MyCanvas.SaveAs("results_temp/" + Name + "_" + str(Mass) + "GeV.png")
+    MyCanvas.SaveAs("results_temp/M2jAvg_" + Name + "_" + str(Mass) + "GeV.png")
 
     ML_pred_val.Draw()
     MyCanvas.SaveAs("results_temp/ML_pred_val_" + Name + "_" + str(Mass) + "GeV.png")
 
     fourjetmasstev.Draw()
-    MyCanvas.SaveAs("results_temp/fourjetmasstev_" + Name + "_" + str(Mass) + "GeV.png")
-    
+    MyCanvas.SaveAs("results_temp/M4jTev_" + Name + "_" + str(Mass) + "GeV.png")
+
     return ML_Accuracy, Truth_Efficiency, ML_Efficiency
 
 #SaveList = [
@@ -91,11 +92,12 @@ def evaluate_rdf (RDF, Name, Mass):
 #]
 
 #InputList = [500]
-#InputList = [500, 600, 700, 800, 900, 1000, 1250, 1500, 1750, 2000, 2500, 3000]
-InputList = ["QCD_1M_stride70"]
+InputList = [500, 600, 700, 800, 900, 1000, 1250, 1500, 1750, 2000, 2500, 3000]
+#InputList = ["QCD_1M_stride70"]
 
 Nbins = len(InputList)
 InputDir = "ML_TTree/"
+SkipGeoCut = True
 
 ML_Accuracy_Trig = ROOT.TH1F("ML_Accuracy_Trig", "ML_Accuracy_Trig", Nbins, 0.5, Nbins + 0.5)
 Truth_Efficiency_Trig = ROOT.TH1F("Truth_Efficiency_Trig", "Truth_Efficiency_Trig", Nbins, 0.5, Nbins + 0.5)
@@ -120,39 +122,39 @@ for Idx, Input in enumerate(InputList):
     MLFile = ROOT.TFile.Open(InputDir + FileName.replace(".root", "_ML.root"))
     MLTree = MLFile.Get("tree_ML")
     MainTree.AddFriend(MLTree)
-    
+
     RDF = ROOT.RDataFrame(MainTree)
-    
+
     RDF = RDF.Filter("evt_trig == 1", "cut_Trig")
-    
+
     RDF = RDF.Define("P1high_M", "P1high_MTeV * 1000")
     RDF = RDF.Define("P1low_M", "P1low_MTeV * 1000")
     RDF = RDF.Define("P2high_M", "P2high_MTeV * 1000")
     RDF = RDF.Define("P2low_M", "P2low_MTeV * 1000")
     RDF = RDF.Define("P3high_M", "P3high_MTeV * 1000")
     RDF = RDF.Define("P3low_M", "P3low_MTeV * 1000")
-    
+
     RDF = RDF.Define("P1_M", "(P1high_M + P1low_M)/2")
     RDF = RDF.Define("P2_M", "(P2high_M + P2low_M)/2")
     RDF = RDF.Define("P3_M", "(P3high_M + P3low_M)/2")
-    
+
     RDF = RDF.Define("P1_QSMD", "TMath::Sq(P1high_M - Mass) + TMath::Sq(P1low_M - Mass)")
     RDF = RDF.Define("P2_QSMD", "TMath::Sq(P2high_M - Mass) + TMath::Sq(P2low_M - Mass)")
     RDF = RDF.Define("P3_QSMD", "TMath::Sq(P3high_M - Mass) + TMath::Sq(P3low_M - Mass)")
-    
+
     RDF = RDF.Define("Truth_QSMD", "min_index(P1_QSMD, P2_QSMD, P3_QSMD)")
     RDF = RDF.Define("ML_pred", "max_index_val(P1_ML, P2_ML, P3_ML).first")
     RDF = RDF.Define("ML_pred_val", "max_index_val(P1_ML, P2_ML, P3_ML).second")
-    
+
     RDF = RDF.Define("Truth_QSMD_M", "col_index<float>(P1_M, P2_M, P3_M, Truth_QSMD)")
     RDF = RDF.Define("ML_pred_M", "col_index<float>(P1_M, P2_M, P3_M, ML_pred)")
-    
+
     print ("Trig cut")
     ML_Accuracy, Truth_Efficiency, ML_Efficiency = evaluate_rdf(RDF, "cut_Trig", Mass)
     ML_Accuracy_Trig.SetBinContent(Idx + 1, ML_Accuracy)
     Truth_Efficiency_Trig.SetBinContent(Idx + 1, Truth_Efficiency)
     ML_Efficiency_Trig.SetBinContent(Idx + 1, ML_Efficiency)
-    
+
     # dR cut
     RDF = RDF.Define("P1JetsVec", "to_vec(P1high_j1, P1high_j2, P1low_j1, P1low_j2)")
     RDF = RDF.Define("P2JetsVec", "to_vec(P2high_j1, P2high_j2, P2low_j1, P2low_j2)")
@@ -160,28 +162,30 @@ for Idx, Input in enumerate(InputList):
     RDF = RDF.Define("ML_pred_Jets", "col_index<std::vector<lvec>>(P1JetsVec, P2JetsVec, P3JetsVec, ML_pred)")
     RDF = RDF.Define("ML_pred_dR1", "ROOT::Math::VectorUtil::DeltaR(ML_pred_Jets[0], ML_pred_Jets[1])")
     RDF = RDF.Define("ML_pred_dR2", "ROOT::Math::VectorUtil::DeltaR(ML_pred_Jets[2], ML_pred_Jets[3])")
-    RDF = RDF.Filter("ML_pred_dR1 < 2 && ML_pred_dR2 < 2", "cut_dR")
-    
-    print ("dR cut")
-    ML_Accuracy, Truth_Efficiency, ML_Efficiency = evaluate_rdf(RDF, "cut_dR", Mass)
-    
+
+    if not SkipGeoCut:
+        RDF = RDF.Filter("ML_pred_dR1 < 2 && ML_pred_dR2 < 2", "cut_dR")
+        print ("dR cut")
+        ML_Accuracy, Truth_Efficiency, ML_Efficiency = evaluate_rdf(RDF, "cut_dR", Mass)
+
     # dEta cut
     RDF = RDF.Define("ML_pred_Phigh_lvec", "ML_pred_Jets[0] + ML_pred_Jets[1]")
     RDF = RDF.Define("ML_pred_Plow_lvec", "ML_pred_Jets[2] + ML_pred_Jets[3]")
-    RDF = RDF.Filter("abs(ML_pred_Phigh_lvec.Eta() - ML_pred_Plow_lvec.Eta()) < 1.1", "cut_dEta")
-    
-    print ("dEta cut")
-    ML_Accuracy, Truth_Efficiency, ML_Efficiency = evaluate_rdf(RDF, "cut_dEta", Mass)
+
+    if not SkipGeoCut:
+        RDF = RDF.Filter("abs(ML_pred_Phigh_lvec.Eta() - ML_pred_Plow_lvec.Eta()) < 1.1", "cut_dEta")
+        print ("dEta cut")
+        ML_Accuracy, Truth_Efficiency, ML_Efficiency = evaluate_rdf(RDF, "cut_dEta", Mass)
 
     # Masym cut
     RDF = RDF.Filter("abs(ML_pred_Phigh_lvec.M() - ML_pred_Plow_lvec.M()) / (ML_pred_Phigh_lvec.M() + ML_pred_Plow_lvec.M()) < 0.1", "cut_Masym")
-    
+
     print ("Masym cut")
     ML_Accuracy, Truth_Efficiency, ML_Efficiency = evaluate_rdf(RDF, "cut_Masym", Mass)
     ML_Accuracy_Masym.SetBinContent(Idx + 1, ML_Accuracy)
     Truth_Efficiency_Masym.SetBinContent(Idx + 1, Truth_Efficiency)
     ML_Efficiency_Masym.SetBinContent(Idx + 1, ML_Efficiency)
-    
+
     CutReport = RDF.Report()
     CutReport.Print()
 
